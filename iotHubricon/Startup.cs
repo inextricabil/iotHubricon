163f5d67ed9a3261ab9b2d32c0f83fb1fd282099
Serban Boghiu <pincellodev14@outlook.com>
2017-10-23 12:56:04 +0300

    Add project files.

---
+++ iotHubricon/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iotHubricon.Startup))]
+namespace iotHubricon
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
