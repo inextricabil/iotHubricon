cb16119199ac8ba0a4fcfc76d25be0611bdea115
Serban Boghiu <pincellodev14@outlook.com>
2017-11-10 18:34:45 +0200

    Migrations + View for Sensor Records

---
+++ iotHubricon/Controllers/SensorRecordsMvcController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using iotHubricon.Models;
+
+namespace iotHubricon.Controllers
+{
+    public class SensorRecordsMvcController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: SensorRecordsMvc
+        public ActionResult Index()
+        {
+            return View(db.SensorRecords.ToList());
+        }
+
+        // GET: SensorRecordsMvc/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SensorRecord sensorRecord = db.SensorRecords.Find(id);
+            if (sensorRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sensorRecord);
+        }
+
+        // GET: SensorRecordsMvc/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SensorRecordsMvc/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "SensorId,Temperature,Humidity")] SensorRecord sensorRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                sensorRecord.SensorId = Guid.NewGuid();
+                db.SensorRecords.Add(sensorRecord);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(sensorRecord);
+        }
+
+        // GET: SensorRecordsMvc/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SensorRecord sensorRecord = db.SensorRecords.Find(id);
+            if (sensorRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sensorRecord);
+        }
+
+        // POST: SensorRecordsMvc/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "SensorId,Temperature,Humidity")] SensorRecord sensorRecord)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(sensorRecord).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(sensorRecord);
+        }
+
+        // GET: SensorRecordsMvc/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            SensorRecord sensorRecord = db.SensorRecords.Find(id);
+            if (sensorRecord == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sensorRecord);
+        }
+
+        // POST: SensorRecordsMvc/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            SensorRecord sensorRecord = db.SensorRecords.Find(id);
+            db.SensorRecords.Remove(sensorRecord);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
